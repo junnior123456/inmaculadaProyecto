42a65c5016f54cba4eb233aa324fa0aee3c88844
junnior marino chinchay macca <junniormarinochinchaymacca@MacBook-Pro-de-junnior.local>
2024-11-07 00:47:51 -0500

    creacion de la estructura del proyecto y las clases model

---
+++ src/main/java/com/inmaculada/inmaculada/InmaculadaApplication.java
@@ -0,0 +1,13 @@
+package com.inmaculada.inmaculada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmaculadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmaculadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inmaculada/inmaculada/InmaculadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmaculada.inmaculada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmaculadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
